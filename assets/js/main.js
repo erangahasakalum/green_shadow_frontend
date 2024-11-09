282ea9bcf954e9fcf48899c8a4144b654023f841
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-09 16:57:46 +0530

    done staff section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 $("#vehicle-sec").css({
     display:"none"
 })
+
+$("#staff-sec").css({
+    display:"none"
+})
 // Handle the vehicle-click action
 $('#vehicle-click').on('click', function() {
     // Empty the main content and move the #vehicle-sec into the .main div
-    $('.main').empty();
+  /*  $('.main').empty();*/
     $('#vehicle-sec').appendTo('.main').show();
-    $('#equipment-sec').hide();  // Hide the equipment section when vehicle is clicked
+    $('#staff-sec').hide();  // Hide the equipment section when vehicle is clicked
+});
+
+// Handle the staff-click action
+$('#staff-click').on('click', function() {
+    // Empty the main content and move the #vehicle-sec into the .main div
+   /* $('.main').empty();*/
+    $('#staff-sec').appendTo('.main').show();
+    $('#vehicle-sec').hide();  // Hide the equipment section when vehicle is clicked
 });
