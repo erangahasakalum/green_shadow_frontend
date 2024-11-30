fbab1e42635874d51fe1e908465f1fcef81c8e89
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-12-01 00:59:31 +0530

    add crop section

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@ $('#vehicle-click').on('click', function() {
     $('#vehicle-sec').appendTo('.main').show();
     $('#staff-sec').hide();  // Hide the equipment section when vehicle is clicked
     $('#equipment-sec').hide();
+    $('#crop-sec').hide();
 });
 
 // Handle the staff-click action
@@ -21,13 +22,25 @@ $('#staff-click').on('click', function() {
     $('#staff-sec').appendTo('.main').show();
     $('#vehicle-sec').hide();  // Hide the equipment section when vehicle is clicked
     $('#equipment-sec').hide();
+    $('#crop-sec').hide();
 });
 
-// Handle the crop-click action
+// Handle the equipment-click action
 $('#equipment-click').on('click', function() {
     // Empty the main content and move the #vehicle-sec into the .main div
     /* $('.main').empty();*/
     $('#equipment-sec').appendTo('.main').show();
     $('#vehicle-sec').hide();  // Hide the equipment section when vehicle is clicked
     $('#staff-sec').hide();
+    $('#crop-sec').hide();
+});
+
+// Handle the crop-click action
+$('#crops-click').on('click', function() {
+    // Empty the main content and move the #vehicle-sec into the .main div
+    /* $('.main').empty();*/
+    $('#crop-sec').appendTo('.main').show();
+    $('#vehicle-sec').hide();  // Hide the equipment section when vehicle is clicked
+    $('#staff-sec').hide();
+    $('#equipment-sec').hide();
 });
