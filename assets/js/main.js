450f8ac22e74f22d35c4d9c681d138fc4db76dc3
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-30 23:58:38 +0530

    add equipment section and staff section

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@ $('#vehicle-click').on('click', function() {
   /*  $('.main').empty();*/
     $('#vehicle-sec').appendTo('.main').show();
     $('#staff-sec').hide();  // Hide the equipment section when vehicle is clicked
+    $('#equipment-sec').hide();
 });
 
 // Handle the staff-click action
@@ -19,4 +20,14 @@ $('#staff-click').on('click', function() {
    /* $('.main').empty();*/
     $('#staff-sec').appendTo('.main').show();
     $('#vehicle-sec').hide();  // Hide the equipment section when vehicle is clicked
+    $('#equipment-sec').hide();
+});
+
+// Handle the crop-click action
+$('#equipment-click').on('click', function() {
+    // Empty the main content and move the #vehicle-sec into the .main div
+    /* $('.main').empty();*/
+    $('#equipment-sec').appendTo('.main').show();
+    $('#vehicle-sec').hide();  // Hide the equipment section when vehicle is clicked
+    $('#staff-sec').hide();
 });
